1750f99fada715bdec3023364ed9ea2e809888c1
Timmz <mohamed.boullouz@gmail.com>
2013-11-16 23:11:05 +0100

    "Algorithm improvements"

--- KataReversi/src/reversi/kata/Reversi.java
+++ KataReversi/src/reversi/kata/Reversi.java
@@ -3,9 +3,10 @@ package reversi.kata;
 public class Reversi {
 	String[] input_to_lines;
 	String[][] lines_to_dots= new String[10][];
-	int enemy_x=-10,enemy_y=-10;
+	Integer[][] enemy_xy=new Integer[8][2];
 	String direction="";
-	
+	int count_nearby=0;
+ 
 	public  String printLegalMoves(String input) {
 		String[] input_to_lines= input.split("\n");	
 		for(int i=0;i<9;i++){
@@ -15,10 +16,13 @@ public class Reversi {
 			for(int j=1;j<7;j++){	 
 				if(lines_to_dots[i][j].equals(".")){
 					if(isEnemyNearby("B",i,j)){
-						getDirection(i,j,this.enemy_x,this.enemy_y);
-					   if(isFriendInDirection()){
+						for(int m=0;m<this.count_nearby;m++){
+						getDirection(i,j,this.enemy_xy[m][0],this.enemy_xy[m][1]);
+					    if(isFriendInDirection(this.enemy_xy[m][0],this.enemy_xy[m][1])){
 						   this.lines_to_dots[i][j]="0";
 					   }
+						}
+						this.count_nearby=0;
 					}
 				}
 			}
@@ -37,63 +41,63 @@ public class Reversi {
 		output=output+"B";
 		return output;
 	}
-    public boolean isFriendInDirection(){
+    public boolean isFriendInDirection(int x,int y){
     	
     	if(this.direction.equals("right")){
-    		int k=this.enemy_y;
-        	int h=this.enemy_x;
+    		int k=y;
+        	int h=x;
 			while(k<8){
 				k++;
-				if(lines_to_dots[this.enemy_x][k].equals("."))
+				if(lines_to_dots[x][k].equals("."))
 					return false;
-				else if(lines_to_dots[this.enemy_x][k].equals("B"))
+				else if(lines_to_dots[x][k].equals("B"))
 					return true;
 				 
 			}
 		}
     	
     	if(this.direction.equals("left")){
-    		int k=this.enemy_y;
-        	int h=this.enemy_x;
+    		int k=y;
+        	int h=x;
 			while(k>0){
 				k--;
-				if(lines_to_dots[this.enemy_x][k].equals("."))
+				if(lines_to_dots[x][k].equals("."))
 					return false;
-				else if(lines_to_dots[this.enemy_x][k].equals("B"))
+				else if(lines_to_dots[x][k].equals("B"))
 					return true;
 				 
 			}
 		}
     	
     	else if(this.direction.equals("up")){
-    		int k=this.enemy_y;
-        	int h=this.enemy_x;
+    		int k=y;
+        	int h=x;
 			while(h>0){
 				h--;
-				if(lines_to_dots[h][this.enemy_y].equals("."))
+				if(lines_to_dots[h][y].equals("."))
 					return false;
-				else if(lines_to_dots[h][this.enemy_y].equals("B"))
+				else if(lines_to_dots[h][y].equals("B"))
 					return true;
 				 
 			}
 		}
     	
-    	if(this.direction.equals("down")){
-    		int k=this.enemy_y;
-        	int h=this.enemy_x;
+    	else if(this.direction.equals("down")){
+    		int k=y;
+        	int h=x;
 			while(h<8){
 				h++;
-				if(lines_to_dots[h][this.enemy_y].equals("."))
+				if(lines_to_dots[h][y].equals("."))
 					return false;
-				else if(lines_to_dots[h][this.enemy_y].equals("B"))
+				else if(lines_to_dots[h][y].equals("B"))
 					return true;
 				 
 			}
 		}
     	
-    	if(this.direction.equals("antislash_up")){
-    		int k=this.enemy_y;
-        	int h=this.enemy_x;
+    	else if(this.direction.equals("antislash_up")){
+    		int k=y;
+        	int h=x;
 			while(h>0 || k>0){
 				k--;
 				h--;
@@ -105,72 +109,80 @@ public class Reversi {
 			}
 		}
     	
-//    	if(this.direction.equals("antislash_down")){
-//			while(h<8 || k<8){
-//				k++;
-//				h++;
-//				if(lines_to_dots[h][k].equals("."))
-//					return false;
-//				else if(lines_to_dots[h][k].equals("B"))
-//					return true;
-//				 
-//			}
-//		}
-//    	
-//    	if(this.direction.equals("slash_down")){
-//			while(h<8 || k>0){
-//				k--;
-//				h++;
-//				if(lines_to_dots[h][k].equals("."))
-//					return false;
-//				else if(lines_to_dots[h][k].equals("B"))
-//					return true;
-//				 
-//			}
-//		}
-//    	
-//    	if(this.direction.equals("slash_up")){
-//			while(h>0 || k<8){
-//				k++;
-//				h--;
-//				if(lines_to_dots[h][k].equals("."))
-//					return false;
-//				else if(lines_to_dots[h][k].equals("B"))
-//					return true;
-//				 
-//			}
-//		}
-//    	
+    	else if(this.direction.equals("antislash_down")){
+     		int k=y;
+        	int h=x;
+ 			while(h<8 || k<8){
+ 				k++;
+ 				h++;
+ 				if(lines_to_dots[h][k].equals("."))
+ 					return false;
+     			else if(lines_to_dots[h][k].equals("B"))
+					return true;
+				 
+			}
+		}
+    	
+    	else if(this.direction.equals("slash_down")){
+    		int k=y;
+        	int h=x;
+			while(h<8 || k>0){
+				k--;
+				h++;
+				if(lines_to_dots[h][k].equals("."))
+					return false;
+				else if(lines_to_dots[h][k].equals("B"))
+					return true;
+				 
+			}
+		}
+    	
+    	else if(this.direction.equals("slash_up")){
+    		int k=y;
+        	int h=x;
+			while(h>0 || k<8){
+				k++;
+				h--;
+				if(lines_to_dots[h][k].equals("."))
+					return false;
+				else if(lines_to_dots[h][k].equals("B"))
+					return true;
+				 
+			}
+		}
+    	
     	return false;
     }
 	public boolean isEnemyNearby(String WorB,int ligne,int colonne) {	
 		for(int i=-1;i<2;i++){
 			for(int j=-1;j<2;j++){
 				if(lines_to_dots[ligne+i][colonne+j].equals("W")){
-					 this.enemy_x=ligne+i;
-					 this.enemy_y=colonne+j;
-					 return true;
+					 this.enemy_xy[this.count_nearby][0]=ligne+i;
+					 this.enemy_xy[this.count_nearby][1]=colonne+j;
+					 this.count_nearby++;
 				}
 			}
 		}
+		if(this.count_nearby>0)
+			 return true;
 		return false;
 	}
 	public void getDirection(int i,int j,int x, int y){
 		if(i==x && y==j-1)
 			this.direction="left";
-		if(i==x && y==j+1)
+		else if(i==x && y==j+1)
 			this.direction="right";
- 		if(x==i-1 && y==j)
+		else if(x==i-1 && y==j)
 			this.direction="up";
-		if(x==i+1 && y==j)
+		else if(x==i+1 && y==j)
 			this.direction="down";
-		if(x==i-1 && y==j-1)
+		else if(x==i-1 && y==j-1)
 			this.direction="antislash_up";
-		if(x==i+1 && y==j+1)
+		else if(x==i+1 && y==j+1)
 			this.direction="antislash_down";
-		if(x==i-1 && y==j+1)
+		else if(x==i-1 && y==j+1)
 			this.direction="slash_up";
-		if(x==i+1 && y==j-1)
+		else if(x==i+1 && y==j-1)
 			this.direction="slash_down";
 			
 	}
--- KataReversi/src/reversi/kata/ReversitUnitTests.java
+++ KataReversi/src/reversi/kata/ReversitUnitTests.java
@@ -8,25 +8,76 @@ public class ReversitUnitTests {
 
 	
 	@Test
-	public void shouldSuccessDirectionAntiSlashUp() {
+	public void shouldSuccessOnDirectionUpDownLeftRight() {
 		String input= 
 				". . . . . . . .\n" +
-				". . B . . . . .\n" +
-				". . . W . . . .\n" +
 				". . . . . . . .\n" +
 				". . . . . . . .\n" +
+				". . . B W . . .\n" +
+				". . . W B . . .\n" +
 				". . . . . . . .\n" +
 				". . . . . . . .\n" +
-				". . . . . . . .\n" +
+				". . . . . . . .\n" + 
 				"B";
 		String output=
 				". . . . . . . . \n" +
-			    ". . B . . . . . \n" +
-			    ". . . W . . . . \n" +
+			    ". . . . . . . . \n" +
 			    ". . . . 0 . . . \n" +
+			    ". . . B W 0 . . \n" +
+			    ". . 0 W B . . . \n" +
+			    ". . . 0 . . . . \n" +
 			    ". . . . . . . . \n" +
 			    ". . . . . . . . \n" +
+			    "B";
+		Reversi game= new Reversi();
+		assertEquals(output,game.printLegalMoves(input));
+	}
+	
+	@Test
+	public void shouldSuccessOnDirectionAntiSlashUp() {
+		String input= 
+				". . . . . . . .\n" +
+				". . . . . . . .\n" +
+				". . . . . . . .\n" +
+				". . . B W . . .\n" +
+				". . . W W . . .\n" +
+				". . . . . . . .\n" +
+				". . . . . . . .\n" +
+				". . . . . . . .\n" + 
+				"B";
+		String output=
+				". . . . . . . . \n" +
 			    ". . . . . . . . \n" +
+			    ". . . . . . . . \n" +
+			    ". . . B W 0 . . \n" +
+			    ". . . W W . . . \n" +
+			    ". . . 0 . 0 . . \n" +
+			    ". . . . . . . . \n" +
+			    ". . . . . . . . \n" +
+			    "B";
+		Reversi game= new Reversi();
+		assertEquals(output,game.printLegalMoves(input));
+	}
+	@Test
+	public void shouldSuccessOnDirectionAntiSlashDown() {
+		String input= 
+				". . . . . . . .\n" +
+				". . . . . . . .\n" +
+				". . . . . . . .\n" +
+				". . . W B . . .\n" +
+				". . . W B . . .\n" +
+				". . . W B . . .\n" +
+				". . . . . . . .\n" +
+				". . . . . . . .\n" + 
+				"B";
+		String output=
+				". . . . . . . . \n" +
+			    ". . . . . . . . \n" +
+			    ". . 0 . . . . . \n" +
+			    ". . 0 W B . . . \n" +
+			    ". . 0 W B . . . \n" +
+			    ". . 0 W B . . . \n" +
+			    ". . 0 . . . . . \n" +
 			    ". . . . . . . . \n" +
 			    "B";
 		Reversi game= new Reversi();
